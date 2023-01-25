e55a08611a191a8e081b213a425e31020171b211
Dvir Cohen <dvir906@gmail.com>
2023-01-26 00:34:29 +0200

    page layout & register fields

--- client/src/layout/Layout/Layout.js
+++ client/src/layout/Layout/Layout.js
@@ -1,20 +1,26 @@
 import React, { Suspense } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 import Container from "@mui/material/Container";
 import classes from "./Layout.module.css";
 
 const Layout = () => {
+  const { pathname } = useLocation();
+  const isAuthRoute =
+    pathname === "/register" || pathname === "/login" ? true : false;
+
+  console.log(pathname);
   return (
     <>
-      <Navbar />
+      {isAuthRoute || <Navbar />}
       <Suspense fallback={<h1>Loading..</h1>}>
         <Container maxWidth="xl" className={classes.layout_container}>
           <Outlet />
         </Container>
       </Suspense>
-      <Footer />
+
+      {isAuthRoute || <Footer />}
     </>
   );
 };
--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -10,6 +10,13 @@ import { loginByEmailAndPassword } from "../../app/redux/slices/authSlice";
 import { joiResolver } from "@hookform/resolvers/joi";
 import AdbIcon from "@mui/icons-material/Adb";
 
+import Grid from "@mui/material/Grid";
+
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+
 const RegisterTextField = ({ label, fieldName, register, errors }) => {
   return (
     <TextField
@@ -20,6 +27,32 @@ const RegisterTextField = ({ label, fieldName, register, errors }) => {
     />
   );
 };
+
+// TODO: create custom component for select
+// const RegisterSelectField = ({ label, fieldName, register, errors,role,handleChange }) => {
+//   return (
+//     <>
+//       {/* <TextField
+//         label={label}
+//         error={Boolean(errors[fieldName])}
+//         helperText={errors[fieldName] ? errors[fieldName]?.message : " "}
+//         {...register(fieldName)}
+//       /> */}
+//       <Select
+//         {...register(fieldName)}
+//         error={Boolean(errors[fieldName])}
+//         helperText={errors[fieldName] ? errors[fieldName]?.message : " "}
+//         value={role}
+//         label={label}
+//         onChange={handleChange}
+//       >
+//         <MenuItem value={10}>Ten</MenuItem>
+//         <MenuItem value={20}>Twenty</MenuItem>
+//         <MenuItem value={"ADMIN"}>ADMIN</MenuItem>
+//       </Select>
+//     </>
+//   );
+// };
 
 const Register = () => {
   const dispatch = useDispatch();
@@ -40,6 +73,12 @@ const Register = () => {
     if (isAuth) navigate("/");
   }, [isAuth, navigate]);
 
+  const [role, setRole] = useState("");
+
+  const handleChange = (event) => {
+    setRole(event.target.value);
+  };
+
   const onRegisterSubmit = (formData) => {
     const formValues = {
       email: formData.email,
@@ -57,52 +96,118 @@ const Register = () => {
     }, 500);
   };
 
+  const pageTitles = {
+    title: "Manage all youe lottery efficiently",
+    subTitle:
+      "Let's get you all set up so you can verify your personal account and begin setting up your profile.",
+  };
+
   return (
     <Box className={classes.page_container}>
       <form onSubmit={handleSubmit(onRegisterSubmit)}>
         <div className={classes.desktop_input_section}>
-          <RegisterTextField
-            label="Email"
-            fieldName="email"
-            register={register}
-            errors={errors}
-          />
-          <RegisterTextField
-            label="Password"
-            fieldName="password"
-            register={register}
-            errors={errors}
-          />
-          <RegisterTextField
-            label="Confirm Password"
-            fieldName="passwordConfirmation"
-            register={register}
-            errors={errors}
-          />
-          <RegisterTextField
-            label="Last Name"
-            fieldName="lastName"
-            register={register}
-            errors={errors}
-          />
-          <RegisterTextField
-            label="First Name"
-            fieldName="firstName"
-            register={register}
-            errors={errors}
-          />
-
-          <RegisterTextField
-            label="Role"
-            fieldName="role"
-            register={register}
-            errors={errors}
-          />
-
-          <Button type="submit" variant="contained">
-            Register
-          </Button>
-          <Link to="/login">Already have an account, please click here</Link>
+          <div className={`titles_container ${classes.top_bottom_margin}`}>
+            <Typography variant="h4" noWrap component="div">
+              Register
+            </Typography>
+            <Typography variant="h6" noWrap component="div">
+              {pageTitles.title}
+            </Typography>
+            <Typography variant="p" noWrap component="div">
+              {pageTitles.subTitle}
+            </Typography>
+          </div>
+          {/* Fields */}
+          <Box sx={{ width: "100%" }}>
+            <Grid
+              container
+              rowSpacing={1}
+              columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+            >
+              <Grid item xs={6}>
+                {" "}
+                <RegisterTextField
+                  label="Email"
+                  fieldName="email"
+                  register={register}
+                  errors={errors}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                {" "}
+                <RegisterTextField
+                  label="Password"
+                  fieldName="password"
+                  register={register}
+                  errors={errors}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                {" "}
+                <RegisterTextField
+                  label="Confirm Password"
+                  fieldName="passwordConfirmation"
+                  register={register}
+                  errors={errors}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                {" "}
+                <RegisterTextField
+                  label="Last Name"
+                  fieldName="lastName"
+                  register={register}
+                  errors={errors}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                {" "}
+                <RegisterTextField
+                  label="First Name"
+                  fieldName="firstName"
+                  register={register}
+                  errors={errors}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                {" "}
+                {/* // TODO: use the select field and use it to select role from ENUM */}
+                {/* <FormControl sx={{ m: 0, width: "82%" }}>
+                  <InputLabel id="role">Role</InputLabel>
+                  <Select
+                    labelId="role"
+                    fieldName="role"
+                    register={register}
+                    errors={errors}
+                    id="role"
+                    value={role}
+                    label="Role"
+                    onChange={handleChange}
+                  >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={"ADMIN"}>ADMIN</MenuItem>
+                  </Select>
+                </FormControl> */}
+                {/* <RegisterTextField
+                  label="Role"
+                  fieldName="role"
+                  register={register}
+                  errors={errors}
+                /> */}
+              </Grid>
+            </Grid>
+          </Box>
+          <Box component={"div"} className={classes.top_bottom_margin}>
+            <Button type="submit" variant="contained">
+              Register
+            </Button>
+            <div>
+              <Link to="/login">
+                Already have an account, please click here
+              </Link>
+            </div>
+          </Box>
         </div>
       </form>
 
@@ -110,20 +215,7 @@ const Register = () => {
         <Box component={"div"} sx={{ display: "flex" }}>
           <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
           <Link to={"/"} style={{ color: "white" }}>
-            <Typography
-              variant="h5"
-              noWrap
-              component="p"
-              sx={{
-                mr: 2,
-                display: { xs: "none", md: "flex" },
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
+            <Typography variant="h5" noWrap component="p">
               GVI
             </Typography>
           </Link>
