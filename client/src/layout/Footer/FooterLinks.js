d1958ca4ad612fec46529cb27d80203d1d4e1a41
Dvir Cohen <dvir906@gmail.com>
2023-01-28 23:45:44 +0200

    clean

--- client/src/layout/Footer/FooterLinks.js
+++ client/src/layout/Footer/FooterLinks.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { List, Box, Typography } from "@mui/material";
+import { List, Box } from "@mui/material";
 import { footerRoutes } from "../../app/_routes";
 import classes from "./Footer.module.css";
 // import { blueGrey } from "@mui/material/colors";
 
 const FooterLinks = () => {
   return (
-    <Box className={"test"}>
+    <Box>
       <List>
         {footerRoutes.map((link, indexId) => {
           return (
