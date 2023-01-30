552a217f0839ec95f560a87c741528fbdbcd7e69
Jayet95 <104586790+Jayet95@users.noreply.github.com>
2023-01-30 15:14:13 +0200

    register logic verification for fields

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,4 +1,4 @@
-export const SOCKET_URL = "http://localhost:9000";
+export const SOCKET_URL = "http://localhost:9001";
 
 export const USER_ROLE = {
   ENTREPRENEUR: "ENTREPRENEUR",
@@ -6,9 +6,11 @@ export const USER_ROLE = {
   ADMIN: "ADMIN",
 };
 
-
 export const USER_PROFILE_LINKS = [
   { label: "Profile", path: "/" },
   { label: "Account", path: "/" },
   { label: "Dashboard", path: "/" },
 ];
+export const emailRegex =
+  /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+export const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
--- client/src/pages/Register/formValidationSchema.js
+++ client/src/pages/Register/formValidationSchema.js
@@ -1,5 +1,5 @@
 import Joi from "joi";
-
+import { passwordRegex, emailRegex } from "../../constants/constants";
 const schema = Joi.object({
   firstName: Joi.string().required().min(2).messages({
     "string.base": `"First Name" should be a type of 'text'`,
@@ -12,29 +12,42 @@ const schema = Joi.object({
     "string.empty": `"Last Name" cannot be an empty`,
     "any.required": `"Last Name" is required `,
   }),
-  email: Joi.string().required().messages({
+  email: Joi.string().pattern(RegExp(emailRegex)).required().messages({
     "string.base": `"email" should be a type of 'email'`,
     "string.empty": `"email" cannot be an empty`,
     "any.required": `"email" is a required field`,
+    "string.pattern.base": `Email must be a valid field`,
   }),
-  password: Joi.string().min(6).max(20).required().messages({
-    "string.base": `"password" should be a type of 'password'`,
-    "string.empty": `"password" cannot be an empty`,
-    "any.required": `"password" is required `,
-    "any.min": `"password" must be more then {#limit}`,
-    "any.max": `"password" must be more then {#limit}`,
-  }),
-  passwordConfirmation: Joi.string().min(6).max(20).required().messages({
-    "string.base": `"Last Name" should be a type of 'text'`,
-    "string.empty": `"Last Name" cannot be an empty`,
-    "any.required": `"Last Name" is required `,
-    "any.min": `"password" must be more then {#limit}`,
-    "any.max": `"password" must be more then {#limit}`,
-  }),
+  password: Joi.string()
+    .pattern(RegExp(passwordRegex))
+    .min(6)
+    .max(20)
+    .required()
+    .messages({
+      "string.base": `"password" should be a type of 'password'`,
+      "string.empty": `"password" cannot be an empty`,
+      "any.required": `"password" is required `,
+      "any.min": `"password" must be more then {#limit}`,
+      "any.max": `"password" must be more then {#limit}`,
+      "string.pattern.base": `password must have 6-20 characaters and must contain at least 1 upper case, 1 lowercase and 1 numeric, between 6 and 20 characters`,
+    }),
+  passwordConfirmation: Joi.string()
+    .min(6)
+    .max(20)
+    .required()
+    .equal(Joi.ref("password"))
+    .regex(RegExp(passwordRegex))
+    .messages({
+      "string.base": `"password" should be a type of 'text'`,
+      "string.empty": `"password" cannot be an empty`,
+      "any.required": `"password" is required `,
+      "any.min": `"password" must be more then {#limit}`,
+      "any.max": `"password" must be more then {#limit}`,
+      "any.only": "confirm password does not match the password field",
+    }),
   role: Joi.string().required().messages({
     "string.base": `"role" should be a type of 'text'`,
-    "string.empty": `"role" cannot be an empty`,
-    "any.required": `"role" is required `,
+    "string.empty": `Please select a Role, this field is required`,
   }),
 });
 
