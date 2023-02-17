d78125aa40e78dfcda753e966f460bbc6f44d32d
hazanshoval <hazanshoval@gmail.com>
2023-02-17 16:31:14 +0200

    update controller+profile design

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {updateUserbyid} from "../../services/user.service"
 import { useForm } from "react-hook-form";
 import {
   Grid,
@@ -19,141 +20,76 @@ function Profile() {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const onSubmit =  async (data) => {
+    console.log(data)
+    await updateUserbyid(data);
   };
+
+  const TextFieldArray=[
+    {label:"First Name",register:"firstName"},
+    {label:"Last Name",register:"lastName"},
+    {label:"Start-Up",register:"startUp"},
+    {label:"Mentoring",register:"mentoring"},
+    {label:"Location",register:"location"},
+    {label:"About",register:"about"},
+    {label:"LNKD",register:"lnkd"},
+    {label:"Facebook",register:"facebook"},
+    {label:"Instagram",register:"instagram"},
+    {label:"GitHub",register:"github"},
+    {label:"Expertise",register:"expertise"},
+  ]
 
   return (
     <Box>
       <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <Typography variant="h3" gutterBottom>
-            Shoval and Shahaf
+        <Grid item xs={12} md={12} lg={6}>
+          <Typography variant="h3" gutterBottom className={classes.title_design}>
+            Update Profile Information
           </Typography>
 
           <form onSubmit={handleSubmit(onSubmit)}>
-            <TextField
+ 
+         {TextFieldArray.map((Table,index)=>{
+          return(
+              <TextField
               className={classes.margin_textfield}
-              id="outlined-firstName"
-              label="First Name"
+              key={index}
+              label={Table.label}
               variant="outlined"
-              {...register("firstName", { required: true })}
-              error={errors.firstName}
-              helperText={errors.firstName && "First Name is required"}
+              {...register(Table.register)}
             />
-            <TextField
-              className={classes.margin_textfield}
-              id="outlined-lastName"
-              label="Last Name"
-              variant="outlined"
-              {...register("lastName", { required: true })}
-              error={errors.lastName}
-              helperText={errors.lastName && "Last Name is required"}
-            />
+          )
+         })}
 
             <TextField
               className={classes.margin_textfield}
-              id="outlined-email"
+              label="Phone Number"
+              variant="outlined"
+              {...register("phoneNumber", { 
+                pattern:/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/i,
+                })}
+              error={errors.phoneNumber}
+              helperText={errors.phoneNumber && "Enter a valid phone number"}
+            />
+             <TextField
+              className={classes.margin_textfield}
               label="Email"
               variant="outlined"
               {...register("email", {
-                required: true,
                 pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
               })}
               error={errors.email}
               helperText={errors.email && "Enter a valid email address"}
-            />
-            <TextField
-              className={classes.margin_textfield}
-              id="outlined-role"
-              label="Role"
-              variant="outlined"
-              {...register("role", { required: true })}
-              error={errors.role}
-              helperText={errors.role && "Role is required"}
-            />
-            <TextField
-              className={classes.margin_textfield}
-              id="outlined-startUp"
-              label="Start-Up"
-              variant="outlined"
-              {...register("startUp")}
-            />
-            <TextField
-              className={classes.margin_textfield}
-              id="outlined-mentoring"
-              label="Mentoring"
-              variant="outlined"
-              {...register("mentoring")}
-            />
-            <TextField
-              className={classes.margin_textfield}
-              id="outlined-location"
-              label="Location"
-              variant="outlined"
-              {...register("location")}
-            />
-            <TextField
-              className={classes.margin_textfield}
-              id="outlined-lnkd"
-              label="LNKD"
-              variant="outlined"
-              {...register("lnkd")}
-            />
-            <TextField
-              className={classes.margin_textfield}
-              id="outlined-facebook"
-              label="Facebook"
-              variant="outlined"
-              {...register("facebook")}
-            />
-            <TextField
-              className={classes.margin_textfield}
-              id="outlined-instagram"
-              label="Instagram"
-              variant="outlined"
-              {...register("instagram")}
-            />
-            <TextField
-              className={classes.margin_textfield}
-              id="outlined-github"
-              label="GitHub"
-              variant="outlined"
-              {...register("github")}
-            />
-            <TextField
-              className={classes.margin_textfield}
-              id="outlined-expertise"
-              label="Expertise"
-              variant="outlined"
-              {...register("expertise")}
-            />
-            <TextField
-              className={classes.margin_textfield}
-              id="outlined-phonenumber"
-              label="Phone Number"
-              variant="outlined"
-              {...register("phoneNumber", { required: true })}
-              error={errors.phoneNumber}
-              helperText={errors.phoneNumber && "Phone Number is required"}
-            />
-            <TextField
-              className={classes.margin_textfield}
-              id="outlined-about"
-              label="About"
-              variant="outlined"
-              {...register("about")}
             />
             <Button variant="contained" type="submit">
               Submit
             </Button>
           </form>
         </Grid>
-
         <Divider orientation="vertical" flexItem>
           VERTICAL
         </Divider>
-        <Grid item xs={5}>
+        <Grid item xs={12} md={6} lg={4}>
           <Paper elevation={3}>
             <Box p={2}>
               <Typography variant="h5" gutterBottom>
